f8dd6635486bc5522711e777995f0e10a8dbf5af
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2016-04-25 21:48:48 -0500

    added basic graphs

--- browser/main.js
+++ browser/main.js
@@ -1,17 +1,76 @@
 var sagent = require('superagent');
+var d3 = require('d3');
 
-function displayData(err, data) {
+function d3fy (person, mmax) {
+  var margin = {top: 20, right: 20, bottom: 30, left: 40},
+  width = 400 - margin.left - margin.right,
+  height = 300 - margin.top - margin.bottom;
+
+  var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
+  var y = d3.scale.linear()
+    .range([height, 0]);
+
+  var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+  var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+  var svg = d3.select('main').append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var data = person.data;
+  x.domain(data.map(function(d) { return d.timeStamp; }));
+  y.domain([0, mmax]);
+
+  svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+  svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", "5px")
+    .style("text-anchor", "end")
+    .text("solved");
+
+  svg.selectAll(".bar")
+    .data(data)
+    .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", function(d) { return x(d.timeStamp); })
+    .attr("width", x.rangeBand())
+    .attr("y", function(d) { return y(d.Solved); })
+    .attr("height", function(d) { return height - y(d.Solved); });
+}
+
+function displayData (err, data) {
   var tar = document.querySelector("#profiles");
   if (err) {
-   return tar.innerHTML = 'There is a problem with the data';
+    return tar.innerHTML = 'There is a problem with the data';
   }
   data = data.body;
+  var mmax = 0;
   for (var i = 0; i < data.length; ++i) {
     var person = data[i];
-    var cur = document.createElement('div');
-    cur.id = person.id;
-    cur.innerHTML = 'Profile of ' + person.id;
-    tar.appendChild(cur);
+    person.data = JSON.parse(person.data);
+    for (var j = 0; j < person.data.length; ++j)
+      mmax = Math.max(mmax, person.data[j].Solved);
+  }
+  for (var i = 0; i < data.length; ++i) {
+    var person = data[i];
+    d3fy(person, mmax);
   }
 }
 
